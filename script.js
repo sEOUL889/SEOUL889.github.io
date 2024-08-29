30d43dd734d8e845b8fd1d004e5b60666410c56d
sEOUL889 <GUINEVERECANTIK03@GMAIL.COM>
2024-08-29 08:57:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ document.querySelector(".tombol").addEventListener('click', function () {
                           })
                         })
                       } else if (result.isDenied) {
-                        Swal.fire('Jahat banget emang ga kangen sama heru', '', 'error').then(function () {
+                        Swal.fire('Jahat banget emang ga kangen sama ABIII', '', 'error').then(function () {
                           Swal.fire('Yaudah deh bye!')
                         })
                       }
